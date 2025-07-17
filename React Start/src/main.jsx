dee831c85c2aaa885662a6d1a9741644deabffc0
Kanak Yadav <yadavkanak109@gmail.com>
2025-07-18 00:34:00 +0530

    clear react boiler plate

--- React Start/src/main.jsx
+++ React Start/src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+import {a,b} from './App.jsx'
+console.log(a , b);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
